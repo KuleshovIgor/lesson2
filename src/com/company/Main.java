c21596f4419298b3dcbfa858bb801be1a806a601
KuleshovIgor <kuleshov.igor.855@gmail.com>
2020-12-12 16:04:30 +0300

    Add IDEA files

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int i= 150;
+        String str = "3500";
+        long i2 = Long.parseLong(str);
+        System.out.println("long i2 = " + i2);
+        String str1 = Integer.toString(i);
+        System.out.println("String n = " + str1);
+
+    }
+
+}
